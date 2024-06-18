17cadc054a78f62e1179c0e6f8f5cc0fb650fbb0
Aleksandar Georgiev <alekskg1993@gmail.com>
2024-06-18 18:38:23 +0300

    integrate mongodb and add cast model and service

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+require('../models/Movie');
+require('../models/Cast');
 
-const connectionString = 'monodb://localhost:27017/movie-magic';
+const connectionString = 'mongodb://localhost:27017/movie-magic';
 
-async function start() {
+async function configDatabase() {
+    await mongoose.connect(connectionString);
 
+    console.log('Database connected');
 }
+
+module.exports = { configDatabase }
--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { home, details, search } = require('../controllers/catalog');
 const { about } = require('../controllers/about');
 const { createGet, createPost } = require('../controllers/movie');
+const { createGet: createCastGet, createPost: createCastPost } = require('../controllers/cast');
 const { notFount } = require('../controllers/404');
 
 
@@ -12,8 +13,10 @@ const router = Router();
 router.get('/', home);
 router.get('/about', about);
 router.get('/details/:id', details);
-router.get('/create', createGet);
-router.post('/create', createPost);
+router.get('/create/movie', createGet);
+router.post('/create/movie', createPost);
+router.get('/create/cast', createCastGet);
+router.post('/create/cast', createCastPost);
 router.get('/search', search);
 
 router.get('*', notFount);
